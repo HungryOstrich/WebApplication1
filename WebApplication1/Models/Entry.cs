84bb5981b45a028102f1eacefc5832cb1c6856ba
kubab <kubab@DESKTOP-K0VM3J6>
2025-11-26 17:19:26 +0100

    Dodaj pliki projektów.

---
+++ WebApplication1/Models/Entry.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int WorkoutId { get; set; }
+        public Workout Workout { get; set; }
+
+        
+        [Required]
+        [Display(Name = "Ćwiczenie")]
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+
+        // Obciążenie w kg
+        [Required]
+        [Range(0, 1000)]
+        [Display(Name = "Ciężar")]
+        public double Weight { get; set; }
+
+        // Liczba serii
+        [Required]
+        [Range(1, 100)]
+        public int Sets { get; set; }
+
+        // Liczba powtórzeń w każdej serii
+        [Required]
+        [Range(1, 200)]
+        [Display(Name = "Powtórzenia")]
+        public int Repetitions { get; set; }
+        
+    }
+}
---
+++ WebApplication1/Models/Workout.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Data i czas rozpoczęcia")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "Data i czas zakończenia")]
+        public DateTime EndTime { get; set; }
+    }
+}
